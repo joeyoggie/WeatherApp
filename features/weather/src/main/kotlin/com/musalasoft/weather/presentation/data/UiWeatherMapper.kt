9783de38538a7808a9801f7b248e3c448788dbf3
Youssef Wagieh <ywagieh-e@jahez.net>
2024-02-05 12:22:13 +0200

    add ui default value validation

--- features/weather/src/main/kotlin/com/musalasoft/weather/presentation/data/UiWeatherMapper.kt
+++ features/weather/src/main/kotlin/com/musalasoft/weather/presentation/data/UiWeatherMapper.kt
@@ -1,18 +1,55 @@
 package com.musalasoft.weather.presentation.data
 
+import com.musalasoft.core.utilities.constants.Constants
 import com.musalasoft.weather.domain.model.CurrentWeather
 import kotlin.math.roundToInt
 
 fun CurrentWeather.toUiWeatherItem() = UiWeatherItem(
     main = this.main,
     description = this.description,
-    temperature = this.temperature.roundToInt().toString().plus("°C"),
-    feelsLike = this.feelsLike.roundToInt().toString().plus("°C"),
-    temperatureMin = this.temperature.roundToInt().toString().plus("°C"),
-    temperatureMax = this.temperature.roundToInt().toString().plus("°C"),
-    humidity = this.humidity.toString().plus("%"),
-    visibility = this.visibility.toString().plus(" KM"),
-    windSpeed = this.windSpeed.toString().plus(" KM/H"),
-    rain = this.rain.toString().plus("%"),
-    clouds = this.clouds.toString()
+    temperature = if (this.temperature != Constants.UNKNOWN_DOUBLE) {
+        this.temperature.roundToInt().toString().plus("°C")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    feelsLike = if (this.feelsLike != Constants.UNKNOWN_DOUBLE) {
+        this.feelsLike.roundToInt().toString().plus("°C")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    temperatureMin = if (this.temperatureMin != Constants.UNKNOWN_DOUBLE) {
+        this.temperatureMin.roundToInt().toString().plus("°C")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    temperatureMax = if (this.temperatureMax != Constants.UNKNOWN_DOUBLE) {
+        this.temperatureMax.roundToInt().toString().plus("°C")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    humidity = if (this.humidity != Constants.UNKNOWN_DOUBLE) {
+        this.humidity.roundToInt().toString().plus("%")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    visibility = if (this.visibility != Constants.UNKNOWN_DOUBLE) {
+        this.visibility.roundToInt().toString().plus(" KM")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    windSpeed = if (this.windSpeed != Constants.UNKNOWN_DOUBLE) {
+        this.windSpeed.roundToInt().toString().plus("KM/H")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    rain = if (this.rain != Constants.UNKNOWN_DOUBLE) {
+        this.rain.roundToInt().toString().plus("%")
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    },
+    clouds = if (this.clouds != Constants.UNKNOWN_DOUBLE) {
+        this.clouds.roundToInt().toString()
+    } else {
+        Constants.EMPTY_STRING_PLACEHOLDER
+    }
 )
